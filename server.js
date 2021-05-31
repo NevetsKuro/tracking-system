fbfc43e8d8b9c2d64ec80d26acf828061c0785f1
stark1996 <stevenfernz96@gmail.com>
2021-05-31 15:30:59 +0530

    changed server.js

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT | 5000;
 const app = express();
 
-
-app.get('/status', (req, res) => { res.status(200).end(); });
-app.head('/status', (req, res) => { res.status(200).end(); });
+app.get('/', (req, res) => { res.send("Server is on!").end(); });
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -19,23 +17,6 @@ require('./models/user');
 require('./models/shares');
 
 app.use(require('./api'));
-
-app.use((req, res, next) => {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res) => {
-  res.status(err.status || 500);
-  res.json({
-    'errors': {
-      message: err.message,
-      error: {}
-    }
-  });
-});
-
 
 app.listen(PORT, err => {
   if (err) {
